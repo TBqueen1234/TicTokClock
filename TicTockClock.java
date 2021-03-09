b00c9202a82fc4f4b65d402ea50331349c945383
TBqueen1234 <80231112+TBqueen1234@users.noreply.github.com>
2021-03-09 18:37:55 -0500

    Add files via upload

---
+++ TicTockClock.java
@@ -0,0 +1,13 @@
+import java.until.Scanner;
+
+public class TicTokClock
+{
+   public static void main(String [] args)
+   {
+      Scanner Computer = new SCanner(System.in);
+      double userGPU = 0; // setting GPU to zero
+      System.out.println(“Enter your clock speed (in Megagnhertz) of the graphics card(GPU)”);
+      userGPU = Computer.nextDouble();
+
+    }
+}
